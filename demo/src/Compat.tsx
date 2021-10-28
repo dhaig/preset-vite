268e6d632d29ab7356011a6bd7024ef2f8e6854b
Maximo Mussini <maximomussini@gmail.com>
2021-10-28 16:17:38 -0300

    chore: clarify comment in hooks demo

--- demo/src/Compat.tsx
+++ demo/src/Compat.tsx
@@ -4,9 +4,13 @@ import React, { useMemo, useState } from "react";
 export function ReactComponent() {
 	const [v, set] = useState(0);
 
-	// NOTE: Test that babel-plugin-transform-hook-names can process this as well.
+	// NOTE: To check in devtools that babel-plugin-transform-hook-names can
+	// extract the variable names from typed expressions.
 	const _unusedState = useState(0 as any);
-	const _unusedMemo = useMemo<number>(() => _unusedState[0], [_unusedState[0]]);
+	const _unusedMemo: number = useMemo<number>(
+		() => _unusedState[0],
+		[_unusedState[0]],
+	);
 
 	return (
 		<div>
